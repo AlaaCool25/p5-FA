92a86133caa24d062d3f122246e08206e5b2b245
AlaaCool25 <97164012+AlaaCool25@users.noreply.github.com>
2022-09-15 20:48:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload() {
+    
+}
+
+function setup() {
+    canvas = createCanvas(300,235);
+    canvas.center();
+
+}
+
+function draw() {
+
+}
+
+function take_snapshot() {
+    save('myFilterImage.png');
+}
